91bcc085ee3f6a53763d06b16106a745f67d38b9
Pranav Shenoy <u1365432@kingspeak1.wasatch.peaks>
2023-04-18 16:52:25 -0600

    mnr

--- src/versions.hpp
+++ src/versions.hpp
@@ -30,9 +30,9 @@ class VisibilityBase {
     vector<vray> process_segments(vector<segment> segments, ofVec2f& q) {
         start = 0;
         std::cout<<"Starting Process"<<std::endl;
-        vector<segment> updated_segments = preprocess(segments, q);
+        vector<segment> updated_segments = this->preprocess(segments, q);
         std::cout<<"Preprocess complete"<<std::endl;
-        vector<vray> merged_vrays = merge(updated_segments);
+        vector<vray> merged_vrays = this->merge(updated_segments);
         std::cout<<"Ending Process"<<std::endl;
         end = 0;
         return merged_vrays;
