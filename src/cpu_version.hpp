c12db34e2b49c78b2bd03bbf936c5de99ca41415
Pranav Shenoy <u1365432@kingspeak1.wasatch.peaks>
2023-04-18 16:58:57 -0600

    mnr

--- src/cpu_version.hpp
+++ src/cpu_version.hpp
@@ -166,13 +166,13 @@ class CPU: public VisibilityBase {
     }
 
     vector<vray> process_segments(vector<segment> segments, ofVec2f& q) {
-        start = 0;
+        // start = 0;
         std::cout<<"Starting Process"<<std::endl;
         vector<segment> updated_segments = this->preprocess(segments, q);
         std::cout<<"Preprocess complete"<<std::endl;
         vector<vray> merged_vrays = this->merge(updated_segments);
         std::cout<<"Ending Process"<<std::endl;
-        end = 0;
+        // end = 0;
         return merged_vrays;
     }
 };
