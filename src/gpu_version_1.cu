7c3cf952db549c3e2a2f161d0355f40882f0b673
Pranav Shenoy <u1365432@kingspeak1.wasatch.peaks>
2023-04-20 16:37:49 -0600

    mnr

--- src/gpu_version_1.cu
+++ src/gpu_version_1.cu
@@ -6,6 +6,160 @@
 using namespace std;
 #define BLOCK_SIZE 512 //@@ You can change this
 
+class vray {
+public:
+    // vray(float _theta, ofVec2f _unitVec, float _r, float _l) :
+    //     theta(_theta), unitVec(_unitVec.x, _unitVec.y), r(_r), l(_l) {}
+    vray(float _theta, float _r, float _l) :
+        theta(_theta), r(_r), l(_l) {}
+    
+    float theta;
+    // ofVec2f unitVec;
+    float r;
+    float l;
+
+};
+
+class segment {
+public:
+    segment(ofVec2f e0, ofVec2f e1) :
+        p0(e0.x, e0.y), p1(e1.x, e1.y) {}
+
+    ofVec2f p0;
+    ofVec2f p1;
+    
+    int signum(float f);
+    void translateToQ(ofVec2f q);
+    bool possibleIntersectionTestXAxis();
+    ofVec2f splitSegmentInto2();
+    ofVec2f intersectionWithGivenSegment(segment other);
+    bool collinearWithQ(); // q is always 0 after translation
+    vector<vray> generateVray(segment seg);
+};
+
+
+
+void segment::translateToQ(ofVec2f pointQ){
+    p0.set(p0.x - pointQ.x, -(p0.y - pointQ.y)); // because display needs oroginal coordinates but geomterically the y direction is opposite
+    p1.set(p1.x - pointQ.x, -(p1.y - pointQ.y));
+}
+
+
+int segment::signum(float f) {
+   if (f > 0) return 1;
+   if (f < 0) return -1;
+   return 0;
+}
+
+
+bool segment::possibleIntersectionTestXAxis(){
+    // TODO: Implement A Method To Find The Intersection Between 2 Axis Aligned Bounding Boxes
+    if(p0.x < 0 && p1.x < 0){
+        return false;
+    }
+    if(p0.y < 0 && p1.y < 0){
+        return false;
+    }
+    if(p0.y > 0 && p1.y > 0){
+        return false;
+    }
+    return true;
+  }
+
+
+ofVec2f segment::splitSegmentInto2(){
+    // TODO: Implement A Fast Method To Find The Edge Intersection Point.
+    // Should return the intersection point or null, if no intersection exists.
+    //  Care should be taken to make the implementation CORRECT, but SPEED MATTERS.
+     
+    segment other(ofVec2f(0.0f, 0.0f), ofVec2f(1500.0f, 0.0f)); // x axis as segment
+    //do the p1 - p0 and use that so that number of multiplication is less
+    float p0x = other.p0.x;
+    float p0y = other.p0.y;
+    float p1x = other.p1.x;
+    float p1y = other.p1.y;
+    float p2x = p0.x;
+    float p2y = p0.y;
+    float p3x = p1.x;
+    float p3y = p1.y;
+    float s = (p2y*p1x - p0y*p1x - p2y*p0x + p0y*p0x - p2x*p1y + p0x*p1y + p2x*p0y - p0x*p0y)/(p3x*p1y - p2x*p1y - p3x*p0y + p2x*p0y - p3y*p1x + p2y*p1x + p3y*p0x - p2y*p0x);
+    if(0 < s && s < 1){ // does not take the end points
+        float t = (p2x + ((p3x - p2x) * s) - p0x)/(p1x - p0x);
+        if(0 < t && t < 1){ // does not take the end points
+             float pointX = p0x + ((p1x - p0x) * t);
+             float pointY = p0y + ((p1y - p0y) * t);
+             return ofVec2f(pointX, pointY);
+        }
+    }
+    return ofVec2f(-1.0f, 0.0f); // if no intersection THEN returned point has x<0
+}
+
+
+ofVec2f segment::intersectionWithGivenSegment(segment other){
+    // TODO: Implement A Fast Method To Find The Edge Intersection Point.
+    // Should return the intersection point or null, if no intersection exists.
+    //  Care should be taken to make the implementation CORRECT, but SPEED MATTERS.
+     
+//    segment other(ofVec2f(0.0f, 0.0f), ofVec2f(1500.0f, 0.0f)); // x axis as segment
+    //do the p1 - p0 and use that so that number of multiplication is less
+    float p0x = other.p0.x;
+    float p0y = other.p0.y;
+    float p1x = other.p1.x;
+    float p1y = other.p1.y;
+    float p2x = p0.x;
+    float p2y = p0.y;
+    float p3x = p1.x;
+    float p3y = p1.y;
+    float s = (p2y*p1x - p0y*p1x - p2y*p0x + p0y*p0x - p2x*p1y + p0x*p1y + p2x*p0y - p0x*p0y)/(p3x*p1y - p2x*p1y - p3x*p0y + p2x*p0y - p3y*p1x + p2y*p1x + p3y*p0x - p2y*p0x);
+    if(0 < s && s < 1){ // does not take the end points
+        float t = (p2x + ((p3x - p2x) * s) - p0x)/(p1x - p0x);
+        if(0 < t && t < 1){ // does not take the end points
+             float pointX = p0x + ((p1x - p0x) * t);
+             float pointY = p0y + ((p1y - p0y) * t);
+             return ofVec2f(pointX, pointY);
+        }
+    }
+    return ofVec2f(0.0f, 0.0f); // if no intersection THEN returned point has x<0, TODO how to pass null
+}
+
+
+bool segment::collinearWithQ(){
+    return abs(p0.x * p1.y - p0.y * p1.x)/2;
+}
+
+
+vector<vray> segment::generateVray(segment seg){
+    vector<vray> bothVray;
+    ofVec2f xAxisVec(1.0,0.0);
+    float infinity = 9999.0f;
+    
+    if((seg.p0.x * seg.p1.y - seg.p0.y * seg.p1.x) < 0){ // p0 X p1
+        // to ensure p0 theta < p1 theta
+        swap(seg.p0, seg.p1);
+    }
+    float theta0 = xAxisVec.angle(seg.p0);
+    float theta1;
+    if(xAxisVec.angle(seg.p1) == 0.0f){
+        theta1 = 360.0;
+    }
+    else{
+        theta1 = xAxisVec.angle(seg.p1);
+    }
+    theta0 = theta0 < 0? theta0 + 360.0f : theta0;
+    theta1 = theta1 < 0? theta1 + 360.0f : theta1;
+    bothVray.push_back(vray(theta0, seg.p0.getNormalized(), infinity, seg.p0.length()));
+    bothVray.push_back(vray(theta1, seg.p1.getNormalized(), seg.p1.length(), infinity));
+    
+    return bothVray;
+}
+
+
+
+
+
+
+
+
 class GPU_V1 {
     float infinity = 9999.0f;
     vector<segment> check_intersections(vector<segment>& segments) {
