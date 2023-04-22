19f4d5bcdcc1a4fd3d97a679121f2fd533d57b86
Pranav Shenoy <u1365432@kingspeak2.wasatch.peaks>
2023-04-22 02:48:41 -0600

    mnr

--- src/gpu_version_1.cu
+++ src/gpu_version_1.cu
@@ -82,20 +82,20 @@ public:
 
 
 
-void segment::translateToQ(ofVec2f pointQ){
+__global__ void segment::translateToQ(ofVec2f pointQ){
     p0.set(p0.x - pointQ.x, -(p0.y - pointQ.y)); // because display needs oroginal coordinates but geomterically the y direction is opposite
     p1.set(p1.x - pointQ.x, -(p1.y - pointQ.y));
 }
 
 
-int segment::signum(float f) {
+__global__ int segment::signum(float f) {
    if (f > 0) return 1;
    if (f < 0) return -1;
    return 0;
 }
 
 
-bool segment::possibleIntersectionTestXAxis(){
+__global__ bool segment::possibleIntersectionTestXAxis(){
     // TODO: Implement A Method To Find The Intersection Between 2 Axis Aligned Bounding Boxes
     if(p0.x < 0 && p1.x < 0){
         return false;
@@ -110,7 +110,7 @@ bool segment::possibleIntersectionTestXAxis(){
   }
 
 
-ofVec2f segment::splitSegmentInto2(){
+  __global__ ofVec2f segment::splitSegmentInto2(){
     // TODO: Implement A Fast Method To Find The Edge Intersection Point.
     // Should return the intersection point or null, if no intersection exists.
     //  Care should be taken to make the implementation CORRECT, but SPEED MATTERS.
@@ -138,7 +138,7 @@ ofVec2f segment::splitSegmentInto2(){
 }
 
 
-ofVec2f segment::intersectionWithGivenSegment(segment other){
+__global__ ofVec2f segment::intersectionWithGivenSegment(segment other){
     // TODO: Implement A Fast Method To Find The Edge Intersection Point.
     // Should return the intersection point or null, if no intersection exists.
     //  Care should be taken to make the implementation CORRECT, but SPEED MATTERS.
@@ -166,12 +166,12 @@ ofVec2f segment::intersectionWithGivenSegment(segment other){
 }
 
 
-bool segment::collinearWithQ(){
+__global__ bool segment::collinearWithQ(){
     return abs(p0.x * p1.y - p0.y * p1.x)/2;
 }
 
 
-vector<vray> segment::generateVray(segment seg){
+__global__ vector<vray> segment::generateVray(segment seg){
     vector<vray> bothVray;
     ofVec2f xAxisVec(1.0,0.0);
     float infinity = 9999.0f;
